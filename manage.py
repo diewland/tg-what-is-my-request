2851a4bfe415ec722ae7695e5b68ccb9c9b0bf4b
diewland <diewland@gmail.com>
2018-08-14 00:14:37 +0700

    clean up code

--- manage.py
+++ manage.py
@@ -1,9 +1,9 @@
 import os
 from telegram.ext import Updater, MessageHandler, Filters
 
+HEROKU_APPNAME = 'tg-what-is-my-request'
 TOKEN = os.environ.get('TOKEN', 'TOKEN')
 PORT = int(os.environ.get('PORT', '5000'))
-
 
 def echo(bot, update):
     update.message.reply_text('Bot answer: ' + update.message.text)
@@ -17,5 +17,5 @@ updater.dispatcher.add_handler(MessageHandler(Filters.text, echo))
 updater.start_webhook(listen="0.0.0.0",
                       port=PORT,
                       url_path=TOKEN)
-updater.bot.setWebhook("https://telegrambotexample31337.herokuapp.com/" + TOKEN)
+updater.bot.setWebhook("https://%s.herokuapp.com/%s" % (HEROKU_APPNAME, TOKEN))
 updater.idle()
